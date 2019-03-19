770c7e2ef87c07d3fed46a45efa785c4a1c9efb6
Paula Tulis <tulis.paula@gmail.com>
2019-03-19 01:26:49 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -127,5 +127,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+dennis_languages = ["C", "Assembly"]
+programmer_hash[:dennis_ritchie][:languages] = dennis_languages
+return programmer_hash
 end
